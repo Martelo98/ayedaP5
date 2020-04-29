5e2dc01f0445d8ab1001e1689777693f089f59d5
Alejandro <alejandromlgranadilla@gmail.com>
2020-04-29 20:36:35 +0100

    quickSort done

--- include/heapSort.hpp
+++ include/heapSort.hpp
@@ -1,20 +1,42 @@
 #include "vector_t.hpp"
 
+
 template<class clave>
-void heapSort(vector_t<clave>& vClave) {
+void baja(int i, vector_t<clave>& vClave, int size) {
+    int h1, h2, h;
 
-    int half = vClave.size() / 2;
-    int aux = 0;
-    for (int i = half; i > 0; i/= 2) {
-        for (int j = i; j < vClave.size(); i++) {
-            aux = vClave[j];
-
-            for (int k = j; k >= half && vClave[k - half] > aux; j -= half) {
-                
-                vClave[k] = vClave[j - half];
-            }
-            vClave[k] = aux;
+    while (2 * i <= size) {
+        h1 = 2*i;
+        h2 = h1 + 1;
+        if (h1 == size) {
+            h = h1;
         }
+        else if (vClave[h1] > vClave[h2]) {
+            h = h1;
+            else {
+                h = h2;
+            }
+        }
+        if (vClave[h] <= vClave[i]) { 
+            break;
+        }
+        else {
+            swap(vClave[i], vClave[h]);
+            i = h;
+        }
+    }
+}
+template<class clave>
+void heapSort(vector_t<clave>& vClave, int size) {
+
+    for (int i = size / 2; i > 0; i--) {
+        baja(i, vClave, size);
+
+    }
+
+    for (int i = n; i > 1; i--) {
+        swap(vClave[1], vClave[i]);
+        baja(1, vClave, i-1);
     }
     
 }
